99dbfe5834ea922139fe999f33673dcfc486690f
thisdjango <tsarkova.dev@gmail.com>
2024-07-22 13:41:41 +0300

    Initial Commit

---
+++ liveCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  liveCell
+//
+//  Created by Diana Tsarkova on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
